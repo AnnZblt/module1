6db111fa407d480a7a61bad899a13c0848918aa3
AnnZblt <ann.zblt@yandex.ru>
2023-02-10 19:33:58 +0600

    added base files

---
+++ index.js
@@ -0,0 +1,11 @@
+const productName = 'PlayStation 5';
+const productAmount = 4;
+const productCategory = 'Game console';
+const productPrice = 500;
+const currency = '$'
+
+let productSum = productAmount * productPrice;
+
+
+console.log('Product name: ', productName);
+console.log('Products sum: ', productSum + currency);
