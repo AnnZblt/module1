f1a2b5c2270480b6d3fe0e135a76039bea8d80b0
AnnZblt <ann.zblt@yandex.ru>
2023-02-13 22:22:04 +0600

    new hw files

---
+++ task1.js
@@ -0,0 +1,7 @@
+const convertMoney = (euro) => {
+    let sumInDollars = euro * 1.2;
+    let sumInRubles = sumInDollars * 64;
+    console.log(`Цена в рублях ${sumInRubles}.`);
+};
+
+convertMoney();
